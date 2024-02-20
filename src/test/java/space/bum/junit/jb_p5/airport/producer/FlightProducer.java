bbf031c5eced01959f7088f9077b1898b30eb435
osparking <jbpark03@gmail.com>
2024-02-20 15:32:32 +0900

    항공편 생산자

---
+++ src/test/java/space/bum/junit/jb_p5/airport/producer/FlightProducer.java
@@ -0,0 +1,17 @@
+package space.bum.junit.jb_p5.airport.producer;
+
+import java.io.IOException;
+
+import javax.enterprise.inject.Produces;
+
+import space.bum.junit.jb_p5.airport.Flight;
+import space.bum.junit.jb_p5.airport.FlightBuilderUtil;
+import space.bum.junit.jb_p5.airport.annot.FlightNumber;
+
+public class FlightProducer {
+  @Produces
+  @FlightNumber(number= "AA1234")
+  public Flight createFlight() throws IOException {
+      return FlightBuilderUtil.buildFlightFromCsv("AA1234", 50,"src/test/resources/flights_information.csv");
+  }
+}
